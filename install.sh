59ab80ff38f714f48229fbf7a1a8f657895bc748
Natsumi Hoshino <me@kagehoshi.com>
2018-05-13 20:52:30 +0900

    add support for git private keys

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ echo "============================="
 
 VERSION=$(curl -L -s -S https://beta.requarks.io/api/version/stable)
 
+echo "-> Copying private key..."
+mkdir ./.ssh
+chmod 700 ./.ssh
+echo $WIKI_GIT_PRIVATE_KEY > ./.ssh/git.pem
+chmod 400 ./.ssh/git.pem
+
 echo "-> Fetching latest build..."
 curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C ./wiki
 
